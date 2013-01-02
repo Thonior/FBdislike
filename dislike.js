09f27c9220d0b9c32383a99d0c1c22f608c8b5b7
jperdior <jperdior@gmail.com>
2013-01-02 18:10:31 +0100

    boton añadido

--- dislike.js
+++ dislike.js
@@ -1,4 +1,7 @@
-$('div[role|="role"]').each(function(index) {
-	//alert($(this));
-	alert('hola');
+$('span[class*="UIActionLinks_bottom"]').each(function() {
+	$(this).append('<span class="dislike" onclick="dislike();">Dislike</span>&nbsp;&middot;&nbsp;');
+});
+
+$('.dislike').click(function(){
+	alert('ola ke ase');
 });
